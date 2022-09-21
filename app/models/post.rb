be95a79a67f94d42b68d1a8106890973743f353d
liz <rhodes.liz@gmail.com>
2022-09-21 15:29:52 -0400

    commit tree 8ec078a7c3469c9e11d4867de26e36ae1896602d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+    validate :title_is_clickbait,
+    :
+end
+
+def title_is_clickbait
+   unless self.title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+   #else self.title != "True Facts"
+    errors.add(:title, "make your title hook 'em")
+   end
 end
